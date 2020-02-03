6ac8d3ceddbe75050d99be9b0e1dd763b1e1eb1d
Sergio-Meier <sergio_meier@hotmail.com>
2020-02-03 21:30:53 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css']
 })
 export class AppComponent  {
-  title = 'Sergio\'s Angular App';
-  
+  // title = 'Sergio\'s Angular App';
+
   constructor() {}
 }
